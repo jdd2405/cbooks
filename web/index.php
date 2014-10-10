484b1e8636b8767e0c1959012bbce14d023497fb
thesi2013 <t547490@hotmail.com>
2014-10-10 10:11:25 +0200

    - website sollte funktionieren - erste select funktion programmiert

--- web/index.php
+++ web/index.php
@@ -5,7 +5,37 @@
  * @package Example-application
  */
 
-require_once 'globals.php';
+require_once 'includes/globals.php';
+require_once 'includes/db.inc.php';
+
+
+  $query = "SELECT * FROM books";  
+  $result = $GLOBALS['mysqli']->query($query);
+  $numberOfRows = mysqli_num_rows($result);
+  
+  $summary= array(array(),array());
+  $counter=0;
+  
+  while($row = mysqli_fetch_array($result)){
+    
+    for($i =0; $i<4;$i++){
+        $summary[$counter][$i]=$row[$i];
+ 
+     }
+     $counter++;
+  }
+
+
+
+  
+  $smarty->assign('array', $summary);
+  $smarty->assign('number', $numberOfRows);
+  $smarty->assign('counter', $counter);
+     
+
+      
+
+
 
 
 $smarty->display('index.tpl');
---
+++ web/templates_c/12945ef7a554a8693865ed556238a419d8d40de4.file.footer.tpl.cache.php
@@ -0,0 +1,24 @@
+<?php /* Smarty version Smarty-3.1.19, created on 2014-09-30 13:28:15
+         compiled from ".\templates\footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:13244542a93cfb92200-11222460%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '12945ef7a554a8693865ed556238a419d8d40de4' => 
+    array (
+      0 => '.\\templates\\footer.tpl',
+      1 => 1411746822,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '13244542a93cfb92200-11222460',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_542a93cfb94087_20766101',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_542a93cfb94087_20766101')) {function content_542a93cfb94087_20766101($_smarty_tpl) {?></BODY>
+</HTML>
+<?php }} ?>
