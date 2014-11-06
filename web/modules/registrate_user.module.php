b0eac0d99d1f8879eb067460bbce392fd583be72
Jonas Däster <schalk.lpd@gmail.com>
2014-11-07 00:42:16 +0100

    new: mail confirmation after registration

--- web/modules/registrate_user.module.php
+++ web/modules/registrate_user.module.php
@@ -11,6 +11,10 @@
  *
  * @author Jonas
  */
+date_default_timezone_set('Europe/Zurich');
+
+require_once('mail/class.phpmailer.php');
+require_once('mail/class.smtp.php');
 require_once 'classes/User.class.php';
 
 class RegistrateUserModule {
@@ -93,6 +97,34 @@ class RegistrateUserModule {
                     $info_msg = "Ihre Registration war erfolgreich. Herzlich Willkommen bei cBooks.ch.";
                     $this->smarty->assign("alert_info", $info_msg);
                     $this->smarty->display("index.tpl");
+
+                    try {
+                        $mail = new PHPMailer();
+                        $mail->IsSMTP();    // Klasse nutzt SMTP
+                        $mail->SMTPAuth = true; // für SMTP Authentifizierung  
+                        $mail->SMTPSecure = "ssl"; // setzt Präfix 
+                        $mail->Host = 'mail.cbooks.ch'; // // setzt GMX als SMTP server
+                        $mail->Port = '465'; // setzt den SMTP port (=> Port 993 ist nötig für eine SSL Verbindung)
+                        $mail->Username = "info@cbooks.ch";
+                        $mail->Password = "r34dB00ks";
+                        $mail->CharSet = 'utf-8';
+                        $mail->SMTPDebug = 0; //    für SMTP debug information (für Testen) 1 = errors and messages 2 = messages only
+                        $body = file_get_contents('templates/registrationMail.html');
+                        $body = stripslashes($body);    // Backslashes enfernt
+                        $mail->SetFrom("info@cbooks.ch", "cBooks.ch - die Büchertauschplatform");     // Sender Information         
+                        $mail->AddAddress($email, "");  // Empfänger information
+                        $mail->AddReplyTo("info@cbooks.ch", "cBooks.ch - die Büchertauschplatform");  // Spezifiziere ReplyTo Addresse
+                        //$mail->AddCC("jonas.daester@gmail.com");   // CC Information 
+                        $mail->Subject = "Herzlich Willkommen bei cBooks.ch";
+                        $mail->AltBody = ""; // Plain-text message body, falls kein HTML email viewer vorhanden
+                        $mail->MsgHTML($body);
+                        $mail->AddAttachment("img/v2.png");      // Attachment
+                        $mail->send();
+                    } catch (phpmailerException $e) {   // PHP Mailer Exception
+                        $e->errorMessage();
+                    } catch (Exception $e) {
+                        echo $e->errorMessage();        // allg. Exception
+                    }
                 }
             }
         } else {
@@ -101,8 +133,8 @@ class RegistrateUserModule {
     }
 
     function updateUser($user) {
-        
-        if(isset($_POST['updateFirstName'], $_POST['updateFamilyName'], $_POST['updateStreet'], $_POST['updateZIP'], $_POST['updateCity'], $_POST['updateEmail'], $_POST['updateTel'])){
+
+        if (isset($_POST['updateFirstName'], $_POST['updateFamilyName'], $_POST['updateStreet'], $_POST['updateZIP'], $_POST['updateCity'], $_POST['updateEmail'], $_POST['updateTel'])) {
             $user->first_name = filter_input(INPUT_POST, 'updateFirstName', FILTER_DEFAULT);
             $user->family_name = filter_input(INPUT_POST, 'updateFamilyName', FILTER_DEFAULT);
             $user->street = filter_input(INPUT_POST, 'updateStreet', FILTER_DEFAULT);
@@ -110,34 +142,30 @@ class RegistrateUserModule {
             $user->city = filter_input(INPUT_POST, 'updateCity', FILTER_DEFAULT);
             $user->email = filter_input(INPUT_POST, 'updateEmail', FILTER_SANITIZE_EMAIL);
             $user->tel = filter_input(INPUT_POST, 'updateTel', FILTER_SANITIZE_NUMBER_INT);
-            
-            
-            
+
+
+
             if ($stmt = $this->mysqli->prepare("
                     UPDATE cb_users 
                     SET email=?, first_name=?, family_name=?, street=?, zip=?, city=?, tel=?
                     WHERE id_cb_user = ?
                 ")) {
-                
+
                 // Bind "$user_id" zum Parameter. 
                 @$stmt->bind_param('ssssisii', $user->email, $user->first_name, $user->family_name, $user->street, $user->zip, $user->city, $user->tel, $user->user_id);
 
                 if ($stmt->execute()) {
                     $this->smarty->assign('alert_success', "Deine Angaben wurden gespeichert.");
                     $this->smarty->display('portal.tpl');
-                }
-                
-                else {
+                } else {
                     $this->smarty->assign('alert_warning', "Da ist etwas schief gegangen.");
                     $smarty->display('portal.tpl');
                 }
-            }
-            else {
+            } else {
                 $this->smarty->assign('alert_warning', "Statement failed");
                 $smarty->display('portal.tpl');
             }
-        }
-        else {
+        } else {
             $this->smarty->assign('alert_warning', "No values inserted.");
             $smarty->display('portal.tpl');
         }
