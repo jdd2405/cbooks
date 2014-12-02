b3b92f1e74e2cf468919ca26127d1db7c6646676
thesi2013 <t547490@hotmail.com>
2014-12-02 19:52:53 +0100

    autor kann geändert werden

--- web/modules/edit_Book.module.php
+++ web/modules/edit_Book.module.php
@@ -24,36 +24,50 @@ class editBook {
         $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
         $subtitle = filter_input(INPUT_POST, 'subtitle', FILTER_SANITIZE_STRING);
         $blurb = filter_input(INPUT_POST, 'blurb', FILTER_SANITIZE_STRING);
-        $isbn = filter_input(INPUT_POST, 'isbn' , FILTER_SANITIZE_NUMBER_INT);
+        $input = filter_input(INPUT_POST, 'isbn' , FILTER_SANITIZE_NUMBER_INT);
+        $isbn = preg_replace("/[^0-9]/","",$input);
         $pbID = filter_input(INPUT_POST, 'pbID', FILTER_SANITIZE_NUMBER_INT);
                       
         $queryUpdateBook = "UPDATE books SET title = '$title', subtitle = '$subtitle', blurb = '$blurb' "
-                . "WHERE id_isbn = $isbn";
+                . "WHERE id_isbn = '$isbn'";
+        $this->mysqli->query($queryUpdateBook);
         
         $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING);
         
         $queryUpdatePersonalBook = "UPDATE personal_books SET description = '$description' "
-                . "WHERE id_personal_book = $pbID";
+                . "WHERE id_personal_book = '$pbID'";
+        $this->mysqli->query($queryUpdatePersonalBook);
+        
         
         $authors = filter_input(INPUT_POST, 'author', FILTER_SANITIZE_STRING);
+        
+        //alte Autorenverlinkungen (vor Update) löschen
+        $queryDelete = "DELETE FROM books_has_authors WHERE books_id_isbn = '$isbn'";
+        $this->mysqli->query($queryDelete);
+        
+        //Update der Autoren
         $author = explode(", ", $authors);
         $anzahlAuthor = count($author);
-        for($i = 0; $i<$anzahlAuthor-1; $i++){
-            $authorInDB = "SELECT id_author FROM authors WHERE aut_name = $author[$i]";
-            if ($result = $this->mysqli->query($authorInDB)){
-                $authorID = $result->fetch_array(MYSQLI_ASSOC);
-                $queryUpdate = "";
+        for($i = 0; $i<$anzahlAuthor; $i++){
+            $authorInDB = "SELECT id_author FROM authors WHERE aut_name = '$author[$i]'";
+            $result = $this->mysqli->query($authorInDB);
+            if (0 !== $result->num_rows){
+                $authorID = $result->fetch_array(MYSQLI_NUM);
+                $result->free();
+                $queryUpdate = "INSERT INTO books_has_authors (books_id_isbn, authors_id_author) VALUES ('$isbn', '$authorID[0]')";
+                $this->mysqli->query($queryUpdate);
+            }
+            else{
+                $queryInsertNewAuthor = "INSERT INTO authors(aut_name) VALUES ('$author[$i]')";
+                $this->mysqli->query($queryInsertNewAuthor);
+                $queryNewAuthorID = "SELECT id_author FROM authors WHERE aut_name = '$author[$i]'";
+                $result = $this->mysqli->query($queryNewAuthorID);
+                $authors_id_author = $result->fetch_array(MYSQLI_NUM);
+                $queryInsertNewBookHasAuthor = "INSERT INTO books_has_authors (books_id_isbn, authors_id_author) VALUES ('$isbn', '$authors_id_author[0]')";
+                $this->mysqli->query($queryInsertNewBookHasAuthor); 
             }
         }
-        
-        //$queryUpdateAuthor = "UPDATE authors SET ";
 
-        $this->mysqli->query($queryUpdateBook);
-        $this->mysqli->query($queryUpdatePersonalBook);
-    /*    
-        Wenn sich der Autor verändert sollen alle Autoren gelöscht.
-     * 
-     */
         header("Location: portal.php?info=Dein Buch wurde geändert.");
         
     }
